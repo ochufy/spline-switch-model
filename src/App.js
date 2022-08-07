3a1755b0754cf2040ba8f07577ca38768f9b1d2c
ochufy <roshancm01@gmail.com>
2022-08-08 00:13:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Spline from '@splinetool/react-spline';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Spline scene="https://prod.spline.design/1TnufordL9PaVM4X/scene.splinecode" />
   );
 }
+
 
 export default App;
